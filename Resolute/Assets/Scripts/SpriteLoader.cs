393aca5bb1c2e3bc932f5a44da1e1859f61ed721
Jonathan Alexander <notdisliked@gmail.com>
2015-05-31 12:48:48 -0700

    Add the Sprite loader and a Player object

---
+++ Resolute/Assets/Scripts/SpriteLoader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteLoader : MonoBehaviour {
+
+    public Sprite texture;
+    private Color[] colors;
+    private int pixelWidth;
+    private int pixelHeight;
+    public GameObject dotFab;
+
+	// Use this for initialization
+	void Start () {
+        colors = texture.texture.GetPixels();
+        pixelWidth = texture.texture.width;
+        pixelHeight = texture.texture.height;
+        int colorIndex = 0;
+        for (int ih = 0; ih < pixelHeight; ih++) {
+            for (int iw = 0; iw < pixelWidth; iw++) {
+                GameObject dot = Instantiate(dotFab) as GameObject;
+                dot.GetComponent<SpriteRenderer>().color = colors[colorIndex];
+                dot.transform.position = new Vector2(iw, ih);
+                dot.transform.SetParent(this.transform, false);
+                colorIndex++;
+            }
+            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
