75e0fbe5cdcfd074814d9de2639526b70780d021
Jonathan Alexander <notdisliked@gmail.com>
2015-06-02 00:26:19 -0700

    Implemented gravity

---
+++ Resolute/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerScript : MonoBehaviour {
+
+    public int speed;
+    public  List<GameObject> pixels = new List<GameObject>();
+
+    void FixedUpdate()
+    {
+        //Get keyboard input for the movement and calculate the player's velocity
+        float horMove = Input.GetAxisRaw("Horizontal") * speed;
+        Vector2 v = GetComponent<Rigidbody2D>().velocity;
+        v.x = horMove;
+        GetComponent<Rigidbody2D>().velocity = v; //Set the velocity
+    }
+}
--- Resolute/Assets/Scripts/SpriteLoader.cs
+++ Resolute/Assets/Scripts/SpriteLoader.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class SpriteLoader : MonoBehaviour {
 
@@ -11,20 +11,31 @@ public class SpriteLoader : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        List<GameObject> dots = new List<GameObject>();
+        PlayerScript script = GetComponent<PlayerScript>();
         colors = texture.texture.GetPixels();
         pixelWidth = texture.texture.width;
         pixelHeight = texture.texture.height;
         int colorIndex = 0;
         for (int ih = 0; ih < pixelHeight; ih++) {
             for (int iw = 0; iw < pixelWidth; iw++) {
+                if (colors[colorIndex].a == 255) {
+
+                }
+                else
+                {
+
+                }
                 GameObject dot = Instantiate(dotFab) as GameObject;
                 dot.GetComponent<SpriteRenderer>().color = colors[colorIndex];
                 dot.transform.position = new Vector2(iw, ih);
                 dot.transform.SetParent(this.transform, false);
                 colorIndex++;
+                dots.Add(dot);
             }
             
         }
+        script.pixels = dots; 
 	}
 	
 	// Update is called once per frame
