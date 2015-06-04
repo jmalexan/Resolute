c4d86a4d254b8433533f413d846f1a69b771c8d1
Jonathan Alexander <notdisliked@gmail.com>
2015-06-04 08:32:55 -0700

    Implemented Pixels falling off.

--- Resolute/Assets/Scripts/HealthScript.cs
+++ Resolute/Assets/Scripts/HealthScript.cs
@@ -7,16 +7,22 @@ public class HealthScript : MonoBehaviour {
     public List<GameObject> pixels = new List<GameObject>();
     public int health;
     private System.Random rand = new System.Random();
+    public GameObject gravityDot;
 
     public void damage(int amnt) {
         for (int i = amnt; i > 0; i--) {
             int intToDrop = rand.Next(pixels.Count - 1);
             GameObject dotToDrop = pixels[intToDrop];
+            GameObject dot = Instantiate(gravityDot) as GameObject;
+            dot.transform.position = dotToDrop.transform.position;
+            Vector2 v = new Vector2(UnityEngine.Random.Range(-3.0f, 3.0f), 5f);
+            dot.GetComponent<SpriteRenderer>().color = dotToDrop.GetComponent<SpriteRenderer>().color;
+            dot.GetComponent<Rigidbody2D>().velocity = v;
             float shadeGrey = UnityEngine.Random.Range(0.0f, 0.5f);
             dotToDrop.GetComponent<SpriteRenderer>().color = new Color(shadeGrey, shadeGrey, shadeGrey, 0.5f);
             pixels.RemoveAt(intToDrop);
+            health--;
         }
-        Debug.Log("Damaged");
     }
 
     public void initHealth(List<GameObject> dots) {
